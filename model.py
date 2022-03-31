c01a971a3c4e7a5168a5ccda96eda07e181a0538
Sarath.P <53684842+sarathp20@users.noreply.github.com>
2022-03-31 14:24:47 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import cv2
-
+import tensorflow as tf
 from keras.models import Model, Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
